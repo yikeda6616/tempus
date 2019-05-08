85f6f331512397ae998c9572a108ff1e7892b278
Yasushi Ikeda <yikeda6616@gmail.com>
2019-05-07 21:49:18 -0700

    Add CORS setting

--- packages/server/server.ts
+++ packages/server/server.ts
@@ -2,10 +2,20 @@ import * as express from 'express';
 
 const app = express();
 
+const PORT = 3001;
+
+// CORSを許可する
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 app.get('/', (req, res) => {
+  console.log(req);
   res.send('hello world');
 });
 
-app.listen(3000, () => {
-  console.log('App listening on port 3000');
+app.listen(PORT, () => {
+  console.log(`App listening on port ${PORT}`);
 });
--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,26 +1,28 @@
 import React from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
 const App: React.FC = () => {
+  function onClick() {
+    axios.get('http://localhost:3001/').then(res => {
+      console.log(res);
+    });
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <p onClick={onClick}>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
     </div>
   );
-}
+};
 
 export default App;

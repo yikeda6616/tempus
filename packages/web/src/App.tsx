94aa651effa1e3aa5ff7dc553a1762c36c1606d1
Yasushi Ikeda <yikeda6616@gmail.com>
2019-05-11 15:28:58 -0700

    Add node-sass and bulma

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,31 +1,11 @@
 import React from 'react';
 import axios from 'axios';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 const App: React.FC = () => {
-  // function onClick() {
-  //   axios.get('http://localhost:3001/').then(res => {
-  //     console.log(res);
-  //   });
-  // }
-
-  async function onClick() {
-    const res = await axios.get('/api');
-    console.log(res);
-  }
-
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p onClick={onClick}>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <header>header here</header>
     </div>
   );
 };

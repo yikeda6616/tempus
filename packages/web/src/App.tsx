1eefdd399d91b5fa2017d5429f157582378cf86b
Yasushi Ikeda <yikeda6616@gmail.com>
2019-05-14 19:56:52 -0700

    Add react-redux

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import axios from 'axios';
-import { Header } from './components/Header/Header';
 
 const App: React.FC = () => {
-  return <Header />;
+  const [todo, setTodo] = useState({ name: '' });
+
+  async function createTodo() {
+    const res = await axios.post('api/todo/', {
+      name: 'halloaxios',
+    });
+    console.log(res);
+  }
+
+  async function getTodo() {
+    const res = await axios.get('api/todo/28c88f21-0337-4398-85b5-69639cfc9ab8');
+    console.log(res.data);
+    setTodo(res.data);
+  }
+
+  return (
+    <div>
+      <p onClick={getTodo}>getTodo: {todo.name}</p>
+      <p onClick={createTodo}>createTodo</p>
+    </div>
+  );
 };
 
 export default App;

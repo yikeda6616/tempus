45605f8f266588f22b84b13a2c2b7894c51aabc8
Yasushi Ikeda <yikeda6616@gmail.com>
2019-05-19 19:22:58 -0700

    Delete unused lines

--- packages/web/src/App.tsx
+++ packages/web/src/App.tsx
@@ -1,25 +1,9 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React from 'react';
 import { getTodo } from './actions/todo';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 
 const App: React.FC = (props: any) => {
-  // const [todo, setTodo] = useState({ name: '' });
-
-  // async function createTodo() {
-  //   const res = await axios.post('api/todo/', {
-  //     name: 'halloaxios',
-  //   });
-  //   console.log(res);
-  // }
-
-  // async function getTodo() {
-  //   const res = await axios.get('api/todo/28c88f21-0337-4398-85b5-69639cfc9ab8');
-  //   console.log(res.data);
-  //   setTodo(res.data);
-  // }
-
   return (
     <div>
       <p onClick={props.getTodo}>getTodo: {props.name}</p>
